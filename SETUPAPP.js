cb18a5558c4f06ef202c6c3f2d8b03824018dff3
Dhinathayaalan <138593418+Dhinathayaalan@users.noreply.github.com>
2024-03-20 12:20:48 +0530

    Create SETUPAPP.js

---
+++ SETUPAPP.js
@@ -0,0 +1,17 @@
+// Importing necessary libraries and components
+    import React from 'react';
+    import Product from './components/Product';
+    import Cart from './components/Cart';
+    
+    // Defining the App component
+    function App() {
+      return (
+        <div className="App">
+          <Product />
+          <Cart />
+        </div>
+      );
+    }
+    
+    export default App;
+     
